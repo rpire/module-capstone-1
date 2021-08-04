e4425a394a71710e41598a377a707b548c4a009c
rpire <rubenpire7@gmail.com>
2021-08-04 18:07:01 -0400

    Apply styling changes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('burger-container').onclick = function () {toggleMenu()};
+document.getElementById('burger-container').onclick = function () { toggleMenu() };
 
 function toggleMenu() {
   document.querySelector('#burger-container').classList.toggle('invisible');
@@ -7,45 +7,82 @@ function toggleMenu() {
 
 const speakers = [
   {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/rpire-img.jpg'],
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/rpire-img.jpg'],
     name: 'Rubén Pire',
     occupation: 'Japanese Teacher at Obelisk Language Center',
-    info: 'Rubén Pire is one of Obelisk\'s English and Japanese. In this ocasion he will be talking about the cultural differences between Japanese and Latin American culture, and explain how each of these cultures can learn from each other.',
+    info: 'One of Obelisk\'s English and Japanese teachers. He will be talking about the cultural differences between Japanese and Latin American culture, and how these cultures can benefit from each other.',
   },
   {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/alopez-img.jpeg'],
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/alopez-img.jpeg'],
     name: 'Antonieta López',
     occupation: 'Former Student at Obelisk Language Center',
-    info: 'Antonieta López is a former student of Obelisk Language Center. She will be talking about the whole Obelisk experience, answering questions about teachers, langauges and courses',
+    info: 'Antonieta is a former student of Obelisk Language Center. She will be talking about the whole Obelisk experience, answering questions about teachers, langauges and courses',
   },
   {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/fmontero-img.jpeg'],
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/fmontero-img.jpeg'],
     name: 'Freddy Montero',
     occupation: 'English Teacher at Obelisk Language Center',
+    info: 'Freddy will be talking about survival English for tourists in English-speaking countries, regular expressions, and frequent questions and answers that will help you survive out there.',
+  },
+  {
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/elopez-img.jpg'],
+    name: 'Edna López',
+    occupation: 'French Teacher at Obelisk Language Center',
     info: 'Freddy Montero will be talking about survival English while traveling through English-speaking countries around the world, regular expressions and frequent questions and answers.',
   },
   {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/alopez-img.jpeg'],
-    name: 'Freddy Montero',
-    occupation: 'English Teacher at Obelisk Language Center',
-    info: 'Freddy Montero will be talking about survival English while traveling through English-speaking countries around the world, regular expressions and frequent questions and answers.',
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/acouttenye-img.jpeg'],
+    name: 'Andrés Couttenye',
+    occupation: 'German Teacher at Obelisk Language Center',
+    info: 'Andrés will be speaking about the rising demand of professionals that it\'s taking place in Germany recently. How learning German could help you land a job will be discussed in this forum',
   },
   {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/alopez-img.jpeg'],
-    name: 'Freddy Montero',
+    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/11-chess-board-gray.png', './media/preview-headshot.jpg'],
+    name: 'Keanu Reeves',
     occupation: 'English Teacher at Obelisk Language Center',
-    info: 'Freddy Montero will be talking about survival English while traveling through English-speaking countries around the world, regular expressions and frequent questions and answers.',
-  },
-  {
-    imagesURL: ['./media/11-chess-board-removebg-preview.png', './media/alopez-img.jpeg'],
-    name: 'Freddy Montero',
-    occupation: 'English Teacher at Obelisk Language Center',
-    info: 'Freddy Montero will be talking about survival English while traveling through English-speaking countries around the world, regular expressions and frequent questions and answers.',
+    info: 'Keanu is our special guest for the event. He will be talking about how music can boost the process of learning a new language and will discuss the different techniques that you can use to learn with music.',
   },
 ];
+
+function htmlGen(num) {
+  for (let i = 0; i < num; i += 1) {
+    const spkCard = `
+      <div class="speaker-card">
+        <img src="#" alt="" class="speaker-deco">
+        <img src="#" alt="Headshot picture" class="speaker-img">
+        <div class="speaker-info">
+          <h3 class="speaker-name"></h3>
+          <h4 class="speaker-occ" ></h4>
+          <hr>
+          <p class="speaker-para"></p>
+        </div>
+      </div>
+    `;
+
+    document.getElementById('speaker-container').innerHTML += spkCard;
+
+    let deco = document.querySelectorAll('.speaker-deco');
+    let img = document.querySelectorAll('.speaker-img');
+    let name = document.querySelectorAll('.speaker-name');
+    let occ = document.querySelectorAll('.speaker-occ');
+    let info = document.querySelectorAll('.speaker-para');
+
+    if (window.innerWidth > 768) {
+      deco[i].setAttribute('src', `${speakers[i].imagesURL[1]}`);
+    } else {
+      deco[i].setAttribute('src', `${speakers[i].imagesURL[0]}`);
+    }
+
+    img[i].setAttribute('src', `${speakers[i].imagesURL[2]}`);
+    name[i].innerHTML = speakers[i].name;
+    occ[i].innerHTML = speakers[i].occupation;
+    info[i].innerHTML = speakers[i].info;
+  }
+}
+
 let btn = document.querySelector('.speaker-btn');
 window.addEventListener('resize', widthTeller);
-btn.onclick = function() {toggleSpeakers()};
+btn.onclick = function () { toggleSpeakers() };
 
 function toggleSpeakers() {
   let btnValue = document.getElementById('morles').innerHTML;
@@ -70,73 +107,12 @@ function spkBtnValid() {
 }
 
 function widthTeller() {
-  console.log(window.innerWidth);
   if (spkBtnValid() || window.innerWidth > 768) {
     document.getElementById('speaker-container').innerHTML = "";
-    for (let i = 0; i < speakers.length; i += 1) {
-      const spkCard = `
-          <div class="speaker-card">
-            <img src="./media/11-chess-board-removebg-preview.png" alt="" id="speaker-deco" class="speaker-deco">
-            <img src="./media/preview-headshot.jpeg" alt="Headshot picture" id="speaker-img" class="speaker-img">
-            <div class="speaker-info">
-              <h3 id="speaker-name">Youchai Benkler</h3>
-              <h4 id="speaker-occ" >Berkman Professor of Entrepeneurial Legal Studies at Harvard Law School</h4>
-              <hr>
-              <p id="speaker-info" >
-                Benkler studies commons-based peer production, and published his
-                seminal book. The Wealth of Networks in 2006.
-              </p>
-            </div>
-          </div>
-      `;
-    
-      document.getElementById('speaker-container').innerHTML += spkCard;
-    
-      let deco = document.querySelectorAll('.speaker-deco');
-      let img = document.querySelectorAll('.speaker-img');
-      let name = document.querySelectorAll('#speaker-name');
-      let occ = document.querySelectorAll('#speaker-occ');
-      let info = document.querySelectorAll('#speaker-info');
-    
-      deco[i].setAttribute('src', `${speakers[i].imagesURL[0]}`);
-      img[i].setAttribute('src', `${speakers[i].imagesURL[1]}`);
-      name[i].innerHTML = speakers[i].name;
-      occ[i].innerHTML = speakers[i].occupation;
-      info[i].innerHTML = speakers[i].info;
-    }
+    htmlGen(6);
   } else {
     document.getElementById('speaker-container').innerHTML = "";
-    for (let i = 0; i < 2; i += 1) {
-      const spkCard = `
-          <div class="speaker-card">
-            <img src="./media/11-chess-board-removebg-preview.png" alt="" id="speaker-deco" class="speaker-deco">
-            <img src="./media/preview-headshot.jpeg" alt="Headshot picture" id="speaker-img" class="speaker-img">
-            <div class="speaker-info">
-              <h3 id="speaker-name">Youchai Benkler</h3>
-              <h4 id="speaker-occ" >Berkman Professor of Entrepeneurial Legal Studies at Harvard Law School</h4>
-              <hr>
-              <p id="speaker-info" >
-                Benkler studies commons-based peer production, and published his
-                seminal book. The Wealth of Networks in 2006.
-              </p>
-            </div>
-          </div>
-      `;
-    
-      document.getElementById('speaker-container').innerHTML += spkCard;
-    
-      let deco = document.querySelectorAll('.speaker-deco');
-      let img = document.querySelectorAll('.speaker-img');
-      let name = document.querySelectorAll('#speaker-name');
-      let occ = document.querySelectorAll('#speaker-occ');
-      let info = document.querySelectorAll('#speaker-info');
-    
-      deco[i].setAttribute('src', `${speakers[i].imagesURL[0]}`);
-      img[i].setAttribute('src', `${speakers[i].imagesURL[1]}`);
-      name[i].innerHTML = speakers[i].name;
-      occ[i].innerHTML = speakers[i].occupation;
-      info[i].innerHTML = speakers[i].info;
-    }
+    htmlGen(2);
   }
   return window.innerWidth;
 }
